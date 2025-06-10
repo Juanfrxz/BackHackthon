b1ca15510e0d475e278823e71a0b78a6245aa92f
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2025-06-10 14:55:00 -0500

    feat: :art: Add new services
    Add services conndb, ratelimit, automapper

--- ApiHabita/Extensions/ApplicationServiceExtensions.cs
+++ ApiHabita/Extensions/ApplicationServiceExtensions.cs
@@ -1,12 +1,52 @@
-using Infrastructure.Repositories; 
-namespace ApiHabita.Extensions; 
-public static class ApplicationServiceExtensions { 
-        public static void ConfigureCors(this IServiceCollection services) => 
-            services.AddCors(options => 
-            { 
-                options.AddPolicy("CorsPolicy", builder => 
-                    builder.AllowAnyOrigin()   // WithOrigins("https://dominio.com") 
-                           .AllowAnyMethod()   // WithMethods("GET","POST") 
-                           .AllowAnyHeader()); // WithHeaders("accept","content-type") 
-            }); 
+using System.Threading.RateLimiting;
+using Application.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.UnitOfWork;
+namespace ApiHabita.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()   // WithOrigins("https://dominio.com") 
+                       .AllowAnyMethod()   // WithMethods("GET","POST") 
+                       .AllowAnyHeader()); // WithHeaders("accept","content-type") 
+        });
+    public static void AddAplicacionServices(this IServiceCollection services)
+    {
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+    public static IServiceCollection AddCustomRateLimiter(this IServiceCollection services)
+            {
+                services.AddRateLimiter(options =>
+                {   
+                    options.OnRejected = async (context, token) =>
+                    {
+                        var ip = context.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "desconocida";
+                        context.HttpContext.Response.StatusCode = 429;
+                        context.HttpContext.Response.ContentType = "application/json";
+                        var mensaje = $"{{\"message\": \"Demasiadas peticiones desde la IP {ip}. Intenta más tarde.\"}}";
+                        await context.HttpContext.Response.WriteAsync(mensaje, token);
+                    };
+
+                    // Aquí no se define GlobalLimiter
+                    options.AddPolicy("ipLimiter", httpContext =>
+                    {
+                        var ip = httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+                        return RateLimitPartition.GetFixedWindowLimiter(ip, _ => new FixedWindowRateLimiterOptions
+                        {
+                            PermitLimit = 5,
+                            Window = TimeSpan.FromSeconds(10),
+                            QueueLimit = 0,
+                            QueueProcessingOrder = QueueProcessingOrder.OldestFirst
+                        });
+                    });
+                    
+                });
+
+                return services;
+        }
 } 
